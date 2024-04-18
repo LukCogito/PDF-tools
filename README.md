bd18954504d7fa3777d8a095fb82364fa345fbac
Luk <luk.cogito@pilsfree.net>
2024-04-18 16:39:39 +0200

    Code made more fool-proof; added spacy import and language model selection; usage of spacy for text processing not implemented yet

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Tools for PDF editing
 
 ## TO DO:
-- Deal with unintended line breaks
+- Deal with unintended line breaks and other stuff; make the text mobi or epub like
     + The ideal output would be txt file where one line is one paragraph of text in original PDF
+    + I have an idea of using spacy for word tokenization and filtering empty line breaks, page numbers, repeating name of authors or chapters; it's not implemented yet
--- extract_txt.py
+++ extract_txt.py
@@ -1,15 +1,43 @@
 # A script for extraction, formating and saving txt from PDF
+# Author:
+#  _          _       ____            _ _        
+# | |   _   _| | __  / ___|___   __ _(_) |_ ___  
+# | |  | | | | |/ / | |   / _ \ / _` | | __/ _ \ 
+# | |__| |_| |   <  | |__| (_) | (_| | | || (_) |
+# |_____\__,_|_|\_\  \____\___/ \__, |_|\__\___/ 
+#                               |___/
 
 import re
+import os
 import sys
-import statistics
+import spacy
 from pdfminer.high_level import extract_text
 
 pdf_path = sys.argv[1]
 txt_path = sys.argv[2]
+lang = sys.argv[3]
+title_threshold = int(sys.argv[4])
 
-if len(sys.argv) != 3:
-    print("Usage: python3 extract_txt.py <pdf_path> <txt_path>")
+if len(sys.argv) != 5:
+    print("Usage: python3 extract_txt.py <pdf_path> <txt_path> <lang> <tittle_threshold>")
+    exit(1)
+
+if not os.path.isdir(pdf_path):
+    print(f"Invalid file path '{pdf_path}'")
+    exit(1)
+
+if lang != "cs" or lang != "en":
+    print("Language must be 'cs' or 'en'.")
+    exit(1)
+
+# Try to load lang_core_web_sm model for nlp
+try:
+    nlp = spacy.load(f"{lang}_core_web_sm")
+# If it fails, just intall it and chill
+except OSError:
+    print(f"Model '{lang}_core_web_sm' is not installed; installing...")
+    spacy.cli.download(f"{lang}_core_web_sm")
+    nlp = spacy.load(f"{lang}_core_web_sm")
 
 def extract_text_from_pdf(pdf_path, txt_path):
     # Load the text from PDF
@@ -48,8 +76,6 @@ def extract_text_from_pdf(pdf_path, txt_path):
         # Lines lengths is list of length of each line in text
         lines_lengths.append(line_length)
 
-    # Title treshold is rounded value of first six-tile of the text length distribution
-    title_threshold = round(statistics.quantiles(lines_lengths, n=6)[0])
     # Create a new string var for output text
     output = ""
 
@@ -57,14 +83,20 @@ def extract_text_from_pdf(pdf_path, txt_path):
     for line, length in zip(text_splitted, lines_lengths):
         # If length of iterrated line equals or is bellow the threshold 
         if length <= title_threshold:
-            # Add it to the output as separate line (we assume that it's title)
-            output += line + "\n"
+            # If output isn't empty
+            if output != "":
+                # Add it to the output as separate line (we assume that it's title)
+                output += "\n" + line + "\n"
+            else:
+                output += line + "\n"
         # In other cases, add it to an existing line
         else:
             output += line + " "
 
     with open(txt_path, "w", encoding="UTF8") as file:
         file.write(output)
+
+    print(f"Text file saved to {txt_path}")
 
 if __name__ == "__main__":
     extract_text_from_pdf(pdf_path, txt_path)
